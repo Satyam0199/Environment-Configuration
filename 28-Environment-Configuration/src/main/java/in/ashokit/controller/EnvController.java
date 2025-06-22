ad8e9caa00f96987456e6c1646fff3556a5ad628
Satyam <satyamr1112@gmail.com>
2025-06-22 22:33:51 +0530

    added project

---
+++ 28-Environment-Configuration/src/main/java/in/ashokit/controller/EnvController.java
@@ -0,0 +1,19 @@
+package in.ashokit.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnvController {
+	
+	@Value("${custom.message}")
+	private String message;
+	
+	@GetMapping("/env")
+	public String getEnvironmentMessage()
+	{
+		return message;
+	}
+
+}
